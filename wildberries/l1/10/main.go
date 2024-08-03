710eb724e97389a354befb3a9ee483ce29f94a36
Charlsie Bolt <charlsib@ob-g7.nsk.21-school.ru>
2024-08-03 17:54:41 +0700

    l1 13,10

--- wildberries/l1/10/main.go
+++ wildberries/l1/10/main.go
@@ -1,10 +1,24 @@
 package main
 
-func groupValues(arr []float32, step float32) map[float32][]float32 {
-	m := make(map[float32][]float32)
+import "fmt"
 
+// import "sort"
+
+func groupValues(arr []float32, step int) map[int][]float32 {
+	m := make(map[int][]float32)
+
+	for _, v := range arr {
+		key := int(v) / step * step
+		if key < 0 {
+			key -= step
+		}
+		m[key] = append(m[key], v)
+	}
+	fmt.Println(m)
+	return m
 }
 
 func main() {
 	arr := []float32{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+	groupValues(arr, 10)
 }
--- wildberries/l1/13/main.go
+++ wildberries/l1/13/main.go
@@ -2,7 +2,18 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Hello World!")
+func swapValues(a *int, b *int) {
+	*a, *b = *b, *a
 }
 
+func main() {
+
+	// #1
+	a := 2
+	b := 3
+	fmt.Println(a, b)
+	swapValues(&a, &b)
+	fmt.Println(a, b)
+	a, b = b, a
+	fmt.Println(a, b)
+}
