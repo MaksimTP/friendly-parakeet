44dc5e851e36c49794323faf78e4e97a7b5b0896
Charlsie Bolt <charlsib@ob-g7.nsk.21-school.ru>
2024-08-03 17:23:12 +0700

    pravki + 8,9 exercises lvl1

--- wildberries/l1/8/main.go
+++ wildberries/l1/8/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
+
+// Дана переменная int64. Разработать программу которая устанавливает i-й бит в 1 или 0.
+
+func setBit(value int64, pos int, toOne bool) int64 {
+	if pos > 63 || pos < 0 {
+		return value
+	}
+	pos = int(math.Pow(2, float64(pos)))
+	if toOne {
+		value |= int64(pos)
+	} else {
+		value |= int64(pos)
+		value ^= int64(pos)
+	}
+	return value
+}
 
 func main() {
-	fmt.Println("Hello World!")
+	var a int64 = 254
+	fmt.Printf("a: %b, binary: %b", a, setBit(a, 1, false))
 }
--- wildberries/l1/9/main.go
+++ wildberries/l1/9/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	in := make(chan int)
+	out := make(chan int)
+
+	go func() {
+		for {
+			res := <-in
+			fmt.Printf("source: %d ", res)
+			out <- res * res
+		}
+	}()
+
+	go func() {
+		for {
+			in <- rand.Int() % 15
+			time.Sleep(time.Second)
+		}
+	}()
+
+	for {
+		fmt.Printf("squared: %d\n", <-out)
+	}
+
 }
