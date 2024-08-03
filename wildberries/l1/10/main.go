0bcdd53bb3c68b42410b7c8957754955c5d1ec1f
Charlsie Bolt <charlsib@ob-g7.nsk.21-school.ru>
2024-08-03 17:43:56 +0700

    l10 nachalo

--- wildberries/l1/10/main.go
+++ wildberries/l1/10/main.go
@@ -1,8 +1,10 @@
 package main
 
-import "fmt"
+func groupValues(arr []float32, step float32) map[float32][]float32 {
+	m := make(map[float32][]float32)
 
-func main() {
-	fmt.Println("Hello World!")
 }
 
+func main() {
+	arr := []float32{-25.4, -27.0, 13.0, 19.0, 15.5, 24.5, -21.0, 32.5}
+}
