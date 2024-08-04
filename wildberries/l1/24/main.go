7773ede89d1c0338dc9283400750c42a97ef7efd
Charlsie Bolt <charlsib@ob-d7.nsk.21-school.ru>
2024-08-04 18:19:22 +0700

    24 lvl1

--- wildberries/l1/24/main.go
+++ wildberries/l1/24/main.go
@@ -2,7 +2,19 @@ package main
 
 import "fmt"
 
-func main() {
-	fmt.Println("Hello World!")
+func remove(arr []int, ind int) ([]int, error) {
+	if ind < 0 || ind >= len(arr) {
+		return arr, fmt.Errorf("index out of range")
+	}
+	return append(arr[:ind], arr[ind+1:]...), nil
 }
 
+func main() {
+	sl := []int{1, 2, 3, 4, 5, 6}
+	sl, err := remove(sl, 5)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Println(sl)
+	}
+}
